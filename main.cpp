6a1e6d4f0e8dca2915d6c4551201c90561d4d4af
Axel Metzinger <axelmetzinger@gmail.com>
2023-03-28 20:51:49 +0000

    Adding comments discribing classes, objects & methods and adding printf

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include <cstdio>
 
 int main()
 {
-    DigitalIn gasDetector(D2);
+    DigitalIn gasDetector(D2);  // DigitalIn is the class, gasDetector the name of the object of this class
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1);  // DigitalOut is the class, alarmLed the name of the object of this class
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown); // gasDetector is an object, .mode() is a methode used on gasDetector
 
-    alarmLed = OFF;
+    alarmLed = OFF;             // alarmLed the name of the object and of is an alias to an int/bool
 
-    while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
-        
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+while (true) {                                                          // while loop, true is a bool
+        if ( gasDetector == ON ) {                                      // gasDetector is an object of the class DigitalIn
+            alarmLed = ON;                                              // alarmLed is an object of the class DigitalOut
+            printf("There is some gas, the alarm is on\n");             // printf is a internal standard method
+        } else {
+            alarmLed = OFF;                                             // alarmLed is an object of the class DigitalOut
+            printf("There is currently no gas, the alarm is off\n");    // printf is a internal standard method
         }
     }
 }
