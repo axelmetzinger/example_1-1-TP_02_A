1f0a034dc4226615dcb87dfd636b54c590f9ab44
Axel Metzinger <axelmetzinger@gmail.com>
2023-03-28 21:09:25 +0000

    Adding some DOxygen documentation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+/**
+ * @file    main.cpp
+ * @brief   Program that switch an alarm LED on if there is gas detected or switch it off otherwise
+ * @author  Axel Metzinger
+ * @date    28/03/2023
+ */
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 #include <cstdio>
+#include <cstdlib>
 
-int main()
-{
+/**
+ * Main function of the program
+ * @return EXIT_SUCCESS if there was no error anything else otherwise
+ */
+int main() {
+    // Creation and initialization of the differents objects
     DigitalIn gasDetector(D2);  // DigitalIn is the class, gasDetector the name of the object of this class
 
     DigitalOut alarmLed(LED1);  // DigitalOut is the class, alarmLed the name of the object of this class
@@ -12,7 +24,8 @@ int main()
 
     alarmLed = OFF;             // alarmLed the name of the object and of is an alias to an int/bool
 
-while (true) {                                                          // while loop, true is a bool
+
+    while (true) {
         if ( gasDetector == ON ) {                                      // gasDetector is an object of the class DigitalIn
             alarmLed = ON;                                              // alarmLed is an object of the class DigitalOut
             printf("There is some gas, the alarm is on\n");             // printf is a internal standard method
@@ -21,4 +34,6 @@ while (true) {                                                          // while
             printf("There is currently no gas, the alarm is off\n");    // printf is a internal standard method
         }
     }
+
+    return EXIT_SUCCESS;    // Supposedly unreachable due to the infinite loop
 }
